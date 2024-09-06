a69ccc83ba57315357e15b52281396ed3ea389c3
kachasan0024 <kana24ballet@icloud.com>
2024-09-06 21:00:05 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let lists = document.getElementsByClassName("pull-down-list");
+const pullDownButton= document.getElementById("lists");
+const pullDownMenu =document.getElementById("pull-down");
 
+pullDownButton.addEventListener("mouseover",function(){
+    pullDownMenu.classList.remove("hidden");
+    console.log("プルダウンメニューが表示されました");
+});
+
+pullDownButton.addEventListener("mouseout",function(){
+    pullDownMenu.classList.add("hidden");
+    console.log("プルダウンメニューが非表示されました")
+});
