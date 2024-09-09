62b10cd9d0805de0c0517c0457f15a149b306312
kachasan0024 <kana24ballet@icloud.com>
2024-09-09 19:23:18 +0900

    イベント発火　基礎編
    JS10

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ pullDownButton.addEventListener("mouseout",function(){
     pullDownMenu.classList.add("hidden");
     console.log("プルダウンメニューが非表示されました")
 });
+
+pullDownButton.addEventListener("mouseover", function(){
+    pullDownButton.setAttribute("style", "background-color:blue;")
+    console.log("マウスオーバー時に青色")
+});
+pullDownButton.addEventListener("click" ,function(){
+    pullDownButton.setAttribute("style","background-color:green;")
+    console.log("クリック時に緑色")
+})
+pullDownButton.addEventListener("mouseout", function(){
+    pullDownButton.removeAttribute("style","background-color:red;")
+    console.log("マウスアウト時に赤色")
+})
